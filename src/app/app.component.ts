29f3ba17c34e0d1a8a7856d831ee97f18ac60afd
piyush132000 <piyush231000@gmail.com>
2021-12-11 12:05:07 +0530

    pure and impure pipe created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+// #docregion
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { HEROES } from '../app/pipe/impure-pipe.pipe';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: ['#flyers, #all {font-style: italic}'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'angular-pure-impure-pipes';
+export class AppComponent implements OnInit {
+  title = 'Flying Heroes (impure pipe)';
+  heroes: {name:string , canFly:boolean}[] = [];
+  canFly = true;
+  mutate = true;
+  constructor() {
+    this.reset();
+  }
+
+  purePipe: boolean = false;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  addHero(name: string) {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    let hero = { name, canFly: this.canFly };
+    if (this.mutate) {
+      // Pure pipe won't update display because heroes array reference is unchanged
+      // Impure pipe will display
+      this.heroes.push(hero);
+      console.log(this.heroes , 'mutate');
+    } else {
+      // Pipe updates display because heroes array is a new object
+      this.heroes = this.heroes.concat(hero);
+      console.log(this.heroes);
+    }
+  }
+
+  reset() {
+    this.heroes = HEROES.slice();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlyingHeroesImpurePipe } from './pipe/impure-pipe.pipe';
+import { PureHeroPipePipe } from './pipe/pure/pure-hero-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlyingHeroesImpurePipe,
+    PureHeroPipePipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
